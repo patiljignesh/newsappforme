c3477182c1b54596d6cc04f1ee294cfb83def037
Jignesh Patil <patiljignesh@gmail.com>
2024-01-15 03:42:51 +0200

    Basic File Setup

---
+++ NewsAppForMe/NewsAppForMe/Controllers/ArticleDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ArticleDetailViewController.swift
+//  NewsAppForMe
+//
+//  Created by Jigneshkumar Patil on 2024/01/15.
+//
+
+import UIKit
+
+class ArticleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NewsAppForMe/NewsAppForMe/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsViewController.swift
+//  NewsAppForMe
+//
+//  Created by Jigneshkumar Patil on 2024/01/15.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
